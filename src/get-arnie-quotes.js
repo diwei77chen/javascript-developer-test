da7394219d572d1e4de825c48310e18a5183860c
diwei77chen <diwei77chen@gmail.com>
2021-03-10 11:47:56 +1100

    Devin's solution

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,32 @@
 const { httpGet } = require('./mock-http-interface');
 
+/**
+ * Formats an Arnie quote response
+ * @param {object} response A http response
+ * @returns {object}
+ */
+const formatArnieQuoteResponse = (response) => {
+  const successResultKey = 'Arnie Quote';
+  const failureResultKey = 'FAILURE';
+  const { status, body } = response || {};
+  const { message } = JSON.parse(body) || {};
+  if (status !== 200) return { [failureResultKey]: message }
+  return { [successResultKey]: message };
+}
+
+/**
+ * Executes a HTTP GET request on each of the URLs, transforms each of the HTTP responses.
+ * @param {string[]} urls The urls to be requested
+ * @returns {Promise} 
+ */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return Promise.allSettled(
+    urls.map((url) => httpGet(url))
+  ).then(
+    (results) => results.map(
+      ({ value } = {}) => formatArnieQuoteResponse(value)
+    )
+  );
 };
 
 module.exports = {
